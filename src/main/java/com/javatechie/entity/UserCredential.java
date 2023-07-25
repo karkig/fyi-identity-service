e1650405cb7295ee9a7f8574021e212a7ed868b6
karkig <kailash.karki4@gmail.com>
2023-07-25 16:48:28 +0530

    first commit

---
+++ src/main/java/com/javatechie/entity/UserCredential.java
@@ -0,0 +1,27 @@
+package com.javatechie.entity;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserCredential {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+}
